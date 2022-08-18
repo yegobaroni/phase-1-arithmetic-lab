746dab6eed28d0e7022be5a95a99221d909d481d
yego <yegokipkemoi254@gmail.com>
2022-08-18 17:48:11 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+let num1 = 12.4
+let num2 = 5;
+let multiply;
+multiply = num1 * num2
+let random = Math.ceil(Math.random());
+let num3 = 34
+let num4 = 10
+let mod;
+mod = num3 % num4
+let max;
+max = Math.max(-10, 2, -7, 17, 5, 20, 4, -9, 7)
